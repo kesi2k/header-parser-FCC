14f25f751433c70aad0550aa6a1664d2cda5acc4
Makesi Hamilton <whadup_hamilton@yahoo.com>
2016-05-27 22:37:56 +0000

    Dealing with favicon request

--- header-parser.js
+++ header-parser.js
@@ -5,19 +5,21 @@ var port = process.env.PORT
 
 http.createServer(function (request, response){
    
-   
+  if (request.url == "/favicon.ico"){} 
    
    var ip_ = request.headers['x-forwarded-for']
    var lang = request.headers["accept-language"].slice(0,5);
    var soft = request.headers["user-agent"];
+   var OS = soft.split('(')[1].split(')')[0];
+   
    
    console.log("This is the OS: ", soft);
    
    
    var headerPa = {
-    ipadress: ip_,  
+    ipaddress: ip_,  
     language: lang,  
-    software: soft   
+    software: OS   
    }
    
    response.writeHead(200, {'Content-Type':'application/json'})
